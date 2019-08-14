0b8d7f067a6cb2045b7aff9691ee04027fd4ebf6
Vladimir Beliaev <vladbel@live.com>
2019-08-14 18:27:25 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, BufWriter};
 use std::os::unix::net::{UnixStream,UnixListener};
 use std::thread;
 
-fn handle_client(stream: UnixStream) {
-    let buffer_reader = BufReader::new(stream);
+fn read_client(buffer_reader: BufReader<UnixStream>) {
     for line in buffer_reader.lines() {
         println!("{}", line.unwrap());
     }
 
-    println!("handle_client exit");
+    println!("buffer exit");
+}
+
+fn send_to_client() {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input);
+    //buffer_writer.write(input.unwrap());
+    println!("send to client {}", input);
 }
 
 fn main() {
@@ -17,7 +23,11 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                thread::spawn(|| handle_client(stream));
+                let buffer_reader = BufReader::new(stream);
+                thread::spawn(|| read_client(buffer_reader));
+
+                //let buffer_writer = BufWriter::new(stream);
+                thread::spawn(|| send_to_client());
             }
             Err(err) => {
                 println!("Error: {}", err);
