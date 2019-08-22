2ed254657c1cdfb5e277a4505d0cf32ba4cff7cd
Vladimir Beliaev <vladbel@live.com>
2019-08-22 16:24:22 -0500

    UDS client: working implementation

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::io::{BufRead, BufReader, BufWriter, Write};
-use std::os::unix::net::{UnixStream,UnixListener};
+use std::os::unix::net::{UnixStream};
 use std::thread;
 use std::sync::{Arc, Mutex};
+use std::env;
+use std::convert::AsRef;
 
 
-fn read_from_client(id: u8, stream: UnixStream) {
+fn read_stream(id: u8, stream: UnixStream) {
     let buffer_reader = BufReader::new(stream);
     for line in buffer_reader.lines() {
         println!("From {}: {}", id, line.unwrap());
@@ -37,15 +39,15 @@ fn read_stdin (arc_mutex: Arc<Mutex<String>>) {
     println!("Exit reading consiole input");
 }
 
-fn send_to_client(id: u8,
+fn write_stream(id: u8,
                   target_stream: UnixStream,
                   arc_mutex: Arc<Mutex<String>>) {
-    
+
     let mut buffer_writer = BufWriter::new(target_stream);
     let mut handled_message = String::new();
 
     loop {
-        std::thread::sleep(std::time::Duration::new(2, 0));
+        std::thread::sleep(std::time::Duration::new(1, 0));
         let local_message = arc_mutex.lock().unwrap();
         if *local_message != handled_message {
             handled_message = local_message.clone();
@@ -57,53 +59,74 @@ fn send_to_client(id: u8,
             }
         }
     }
-
     println!("Exit reading consiole input");
 }
 
+
+enum EndPointType {
+    UdsServer,
+    UdsClient
+}
+struct EndPointConfiguration {
+    end_point_type: EndPointType,
+    address: String
+}
+
 fn main() {
-    let listener = UnixListener::bind("/tmp/rust_ipc_socket").unwrap();
+    let args: Vec<String> = env::args().collect();
+    let mut end_points = Vec::<EndPointConfiguration>::new();
 
-    let arc_message = Arc::new(Mutex::new(<String>::new()));
-
-     
-    let arc_message_ref = Arc::clone(&arc_message);
-    thread::spawn(move || {
-        read_stdin( arc_message_ref );
-    });
-
-    let arc_message_ref_2 =Arc::clone(&arc_message);
-
-    thread::spawn(move || {
-        let mut handled_message = String::new();
-        loop {
-            std::thread::sleep(std::time::Duration::new(2, 0));
-            //println!("Read shared resource: trying to aquire Mutex on messge");
-            let local_message = arc_message_ref_2.lock().unwrap();
-            if *local_message != handled_message {
-                handled_message = local_message.clone();
-                println!("new message = {}", local_message);
+    for (i, arg) in args.iter().enumerate() {
+        match arg.as_ref() {
+            "--uds_client" => {
+                if i + 1 < args.len() {
+                    let copy = args[i + 1].clone();
+                    end_points.push ( EndPointConfiguration{end_point_type: EndPointType::UdsClient,
+                                                        address: copy});
+                }
             }
-        }
-    });
-
-    let mut client_num: u8 = 0;
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                client_num = client_num + 1;
-                println!("New incoming stream");
-                let stream1 = stream.try_clone().unwrap();
-                let arc_message_ref_3 =Arc::clone(&arc_message);
-                thread::spawn(move || send_to_client(client_num, stream1, arc_message_ref_3));
-
-                thread::spawn(move || read_from_client(client_num, stream));
-
+            "--uds_server" => {
+                if i + 1 < args.len() {
+                    let copy = args[i + 1].clone();
+                    end_points.push ( EndPointConfiguration{end_point_type: EndPointType::UdsServer,
+                                                        address: copy});
+                }
             }
-            Err(err) => {
-                println!("Error: {}", err);
-                break;
+            _ => {
+                // Noop
             }
         }
     }
+
+    let arc_message = Arc::new(Mutex::new(<String>::new()));
+    let arc_message_ref = Arc::clone(&arc_message);
+
+    let user_input_thread = thread::spawn(move || {
+        read_stdin( arc_message_ref );
+    });
+
+    let mut client_num: u8 = 0;
+
+    for end_point in end_points {
+        match end_point.end_point_type {
+            EndPointType::UdsClient => {
+                match UnixStream::connect(end_point.address) {
+                    Ok(stream) => {
+                        client_num = client_num + 1;
+                        let stream1 = stream.try_clone().unwrap();
+                        let arc_message_ref_3 =Arc::clone(&arc_message);
+                        thread::spawn(move || write_stream(client_num, stream1, arc_message_ref_3));
+                        thread::spawn(move || read_stream(client_num, stream));
+                    }
+                    Err(err) => {
+                        println!("Error: {}", err);
+                    }
+                }
+            }
+            EndPointType::UdsServer => {
+            }
+        }
+    }
+
+    user_input_thread.join().expect("user_input_thread paniced");
 }
