633a3f3db886dba1d91ee3af0676da4db4a0c571
Vladimir Beliaev <vladbel@live.com>
2019-08-28 13:14:10 -0500

    wip: working version with mpsc message broker

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,17 @@ use std::convert::AsRef;
 use std::sync::mpsc::channel;
 use std::sync::mpsc::{Sender, Receiver};
 
-fn read_stream(stream: UnixStream, tx: Sender<String>) {
+fn read_stream(id: u8, stream: UnixStream, tx: Sender<ChannelMessage>) {
     let buffer_reader = BufReader::new(stream);
+
     for line in buffer_reader.lines() {
         let data_to_send = line.unwrap();
-        println!("Recieved: {}", data_to_send);
-        tx.send(data_to_send).unwrap();
+        let message = ChannelMessage { sender_id: id, text: data_to_send };
+        tx.send(message).unwrap();
     }
-
-    println!("buffer exit");
 }
 
-fn read_stdin (tx: Sender<String>) {
+fn read_stdin (id: u8, tx: Sender<ChannelMessage>) {
 
     let mut read_input = true;
     while read_input {
@@ -30,7 +29,8 @@ fn read_stdin (tx: Sender<String>) {
                 if input == "exit\n" {
                     read_input = false;
                 }
-                tx.send(input).unwrap();
+                let message = ChannelMessage { sender_id: id, text: input };
+                tx.send(message).unwrap();
             }
             Err(err) => {
                     println!("Error: {}", err);
@@ -41,25 +41,30 @@ fn read_stdin (tx: Sender<String>) {
     println!("Exit reading consiole input");
 }
 
-fn write_stream(rx: Receiver<String>, 
+fn write_stream(rx: Receiver<ChannelMessage>, 
                 target_stream: UnixStream) {
 
     let mut buffer_writer = BufWriter::new(target_stream);
 
     loop {
         let message = rx.recv().unwrap();
-        println!("Send new message = {}", message);
-        buffer_writer.write(message .as_bytes()).unwrap();
+        buffer_writer.write("//text start\n" .as_bytes()).unwrap();
+        buffer_writer.write(message.text .as_bytes()).unwrap();
+        buffer_writer.write("//text end\n" .as_bytes()).unwrap();
+
         buffer_writer.flush().unwrap();
     }
 }
 
+const ID_ALL: u8 = 0;
+const ID_ZIPGATEWAY: u8 = 10;
+const ID_HUBCORE: u8 = 12;
+const ID_STDIO: u8 = 14;
 
 enum EndPointType {
     None,
     UdsServer,
-    UdsClient,
-    Stdio
+    UdsClient
 }
 
 struct EndPointConfiguration {
@@ -67,16 +72,46 @@ struct EndPointConfiguration {
     address: String
 }
 
-struct SendTargetChannel{
-    tx: std::sync::mpsc::Sender<String>,
+// ****************************************************************
+struct ChannelMessage {
+    sender_id: u8,
+    subscribers: Vec<u8>,
+    //raw_data: Vec::<u8>,
+    text: String
+}
+
+impl ChannelMessage {
+    fn clone(&self) -> ChannelMessage {
+        return ChannelMessage { 
+            sender_id: self.sender_id, 
+            subscribers: self.subscribers.clone(), 
+            text: self.text.clone()
+        };
+    }
+
+    fn is_subscribed( &self, subscriber_id: u8) -> bool {
+        let i = self.subscribers.enumerate()
+            .find(|&r| {
+                (r == subscriber_id) | (r == ID_ALL);
+            }.unwrap().0
+        return i >= 0;
+    }
+}
+
+//  Subscriber channel
+
+struct SubscriberChannel {
+    tx: std::sync::mpsc::Sender<ChannelMessage>,
     id: u8
 }
 
-const ID_ZIPGATEWAY: u8 = 10;
-const ID_HUBCORE: u8 = 12;
-const ID_STDIO: u8 = 14;
-
-
+impl SubscriberChannel {
+    fn send (&self, message: ChannelMessage) {
+        if self.id != message.sender_id {
+            self.tx.send(message).unwrap();
+        }
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -111,62 +146,18 @@ fn main() {
                 }
             }
             _ => {
-
+                // noop
             }
         }
 
     }
 
-    let (tx_zipgateway, rx_zipgateway) = channel();
-    let (tx_hubcore, rx_hubcore) = channel();
-    let (tx_stdio, rx_stdio) = channel();
+        // init brocker TX channels
+    let mut brocker_tx_channels = Vec::<SubscriberChannel>::new();
     let (tx_brocker, rx_brocker) = channel();
 
-    // init brocker TX channels
-    let mut brocker_tx_channels = Vec::<SendTargetChannel>::new();
-    brocker_tx_channels.push( SendTargetChannel{id: ID_ZIPGATEWAY, tx: tx_zipgateway.clone()});
-    brocker_tx_channels.push( SendTargetChannel{id: ID_HUBCORE, tx: tx_hubcore.clone()});
-    brocker_tx_channels.push( SendTargetChannel{id: ID_STDIO, tx: tx_stdio.clone()});
-
-        // read user input
-    let tx_brocker_clone_1 = tx_brocker.clone();
-    let user_input_thread = thread::spawn(move || {
-        read_stdin(tx_brocker_clone_1);
-    });
-
-
-    // brocker
-    thread::spawn(move|| {
-        loop {
-            let recieved = rx_brocker.recv().unwrap();
-            println!("Recieved: {}", recieved);
-            for target in &brocker_tx_channels {
-                target.tx.send(recieved.clone()).unwrap();
-            }
-        }
-    });
-
-     // Zip Gateway end point
-    match zipgateway_client.end_point_type
-    {
-        EndPointType::UdsClient => {
-            match UnixStream::connect(zipgateway_client.address) {
-                Ok(stream) => {
-                    let tx_brocker_clone_2 = tx_brocker.clone();
-                    let stream_clone = stream.try_clone().unwrap();
-                    thread::spawn(move || write_stream(rx_zipgateway, stream_clone));
-                    thread::spawn(move || read_stream(stream, tx_brocker_clone_2));
-                }
-                Err(err) => {
-                    println!("Error: {}", err);
-                }
-            }
-        }
-        _ => {
-            // noop
-        }
-    }
-
+    // init hub-core
+    // TODO
     // will block the thread
     // should be moved from main thread
     match hub_core_server.end_point_type {
@@ -180,11 +171,15 @@ fn main() {
 
                         let tx_brocker_clone_3 = tx_brocker.clone();
 
-                        // TODO: 
-                        // let stream1 = stream.try_clone().unwrap();
-                        // thread::spawn(move || write_stream(rx_hubcore, stream1));
 
-                        thread::spawn(move || read_stream(stream, tx_brocker_clone_3));
+                        let (tx_hubcore, rx_hubcore) = channel();
+                        brocker_tx_channels.push( SubscriberChannel{id: ID_HUBCORE, tx: tx_hubcore.clone()});
+
+                        let stream1 = stream.try_clone().unwrap();
+                        thread::spawn(move || write_stream(rx_hubcore, stream1));
+
+                        thread::spawn(move || read_stream(ID_HUBCORE,  stream, tx_brocker_clone_3));
+                        break;
                     }
                     Err(err) => {
                         println!("Error: {}", err);
@@ -193,9 +188,84 @@ fn main() {
             }
         }
         _ => {
-            // no op
+            // noop
+        }
+    }
+    //
+
+    let (tx_zipgateway, rx_zipgateway) = channel();
+    
+    let (tx_stdio, rx_stdio) = channel();
+
+
+    brocker_tx_channels.push( SubscriberChannel{id: ID_ZIPGATEWAY, tx: tx_zipgateway.clone()});
+    brocker_tx_channels.push( SubscriberChannel{id: ID_STDIO, tx: tx_stdio.clone()});
+
+
+    // brocker
+    thread::spawn(move|| {
+        loop {
+            let recieved = rx_brocker.recv().unwrap();
+            println!("Recieved by message brocker : {}", recieved.text);
+
+            for subscriber in &brocker_tx_channels {
+                subscriber.send(recieved.clone());
+            }
+        }
+    });
+
+     // Zip Gateway end point
+    match zipgateway_client.end_point_type
+    {
+        EndPointType::UdsClient => {
+            match UnixStream::connect(zipgateway_client.address) {
+                Ok(stream) => {
+                    let tx_brocker_clone_2 = tx_brocker.clone();
+                    let stream_clone = stream.try_clone().unwrap();
+                    thread::spawn(move || write_stream(rx_zipgateway, stream_clone));
+                    thread::spawn(move || read_stream(ID_ZIPGATEWAY, stream, tx_brocker_clone_2));
+                }
+                Err(err) => {
+                    println!("Error: {}", err);
+                }
+            }
+        }
+        _ => {
+            // noop
         }
     }
 
-    user_input_thread.join().expect("user_input_thread paniced");
+    // Init stdio
+    // std output
+    thread::spawn(move || {  // read standard input and send it to brocker channel
+        loop {
+            let message = rx_stdio.recv().unwrap();
+            match message.sender_id {
+                ID_HUBCORE => {
+                    println! (" Recieved from HUB: {} .", message.text)
+                }
+                ID_ZIPGATEWAY => {
+                    println! (" Recieved from ZIPGATEWAY: {} .", message.text)
+                }
+                ID_STDIO => {
+                    println! (" This should never happen: sent from STDIO to STDIO. ")
+                }
+                _ => {
+                    println! (" This should never happen: unknown sender ")
+                }
+            }
+            println! ()
+        }
+    });
+
+
+    // read user input
+    let tx_brocker_clone_1 = tx_brocker.clone();
+    let std_input_thread = thread::spawn(move || {  // read standard input and send it to brocker channel
+        read_stdin( ID_STDIO,  tx_brocker_clone_1);
+    });
+
+    
+
+    std_input_thread.join().expect("user_input_thread paniced");
 }
